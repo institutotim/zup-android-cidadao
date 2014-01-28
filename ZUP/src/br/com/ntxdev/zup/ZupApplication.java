c66492e5928111afd3a5528081afd86d52f6bfbd
Rafael Toledo <rafaeldtoledo@gmail.com>
2014-01-27 22:28:27 -0200

    Reativando Sentry

--- ZUP/src/br/com/ntxdev/zup/ZupApplication.java
+++ ZUP/src/br/com/ntxdev/zup/ZupApplication.java
@@ -16,10 +16,10 @@ public class ZupApplication extends Application {
     public void onCreate() {
         super.onCreate();
 
-//        ACRA.init(this);
-//        SentrySender sentry = new SentrySender("https://70310cf77e7a458d853f077510ac44ad:d0bbcb5db6994c3db636aea2a02379c2@app.getsentry.com/17177");
-//        ACRA.getErrorReporter().setReportSender(sentry);
-//        ACRA.getErrorReporter().checkReportsOnApplicationStart();
+        ACRA.init(this);
+        SentrySender sentry = new SentrySender("https://70310cf77e7a458d853f077510ac44ad:d0bbcb5db6994c3db636aea2a02379c2@app.getsentry.com/17177");
+        ACRA.getErrorReporter().setReportSender(sentry);
+        ACRA.getErrorReporter().checkReportsOnApplicationStart();
 
         context = this.getApplicationContext();
     }
