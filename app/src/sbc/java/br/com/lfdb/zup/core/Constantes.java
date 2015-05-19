72591d01c71bb3ffd6ca9bb871c4ef43d5eeba61
LaFabbrica <marcio@lfdb.com.br>
2015-05-19 03:24:42 +0000

    Release 05-19-15

--- app/src/sbc/java/br/com/lfdb/zup/core/Constantes.java
+++ app/src/sbc/java/br/com/lfdb/zup/core/Constantes.java
@@ -6,8 +6,8 @@ public class Constantes {
 	public static final String PREF_KEY_OAUTH_TOKEN = "oauthToken";
     public static final String PREF_KEY_OAUTH_SECRET = "oauthTokenSecret";
     
-    public static final String REST_URL = "http://zuphmg.saobernardo.sp.gov.br:8282";
-    public static final String WEBSITE_URL = "http://zuphmg.saobernardo.sp.gov.br:8080/web";
+    public static final String REST_URL = "http://vcsbc.saobernardo.sp.gov.br:8081";
+    public static final String WEBSITE_URL = "http://vcsbc.saobernardo.sp.gov.br:8081/web";
     public static final String PLACES_KEY = "AIzaSyCOixuls1j6rqhYizrTjw4jymX_T23KTjw";
 
     public static final double INITIAL_LATITUDE = -23.7031669;
