87dcbb3f81134542596f00b4fcd81754c419846b
vitor-kato <vitor.s.kato@gmail.com>
2018-01-23 15:33:28 -0200

    Added flavours

---
+++ app/src/particity/java/br/com/lfdb/zup/core/Constantes.java
@@ -0,0 +1,16 @@
+package br.com.lfdb.zup.core;
+
+public class Constantes {
+
+    public static final String PREF_KEY_TWITTER_LOGIN = "twitterLogado";
+    public static final String PREF_KEY_OAUTH_TOKEN = "oauthToken";
+    public static final String PREF_KEY_OAUTH_SECRET = "oauthTokenSecret";
+
+    public static final String REST_URL = "http://particity.cascavel.pr.gov.br:8282";
+    public static final String WEBSITE_URL = "http://particity.cascavel.pr.gov.br";
+    public static final String PLACES_KEY = "AIzaSyCU4b3Ct3TydDXtLLxT38MXAhlf3OkYfrg";
+
+    public static final String NAMESPACE_DEFAULT = "1";
+    public static final double INITIAL_LATITUDE = -24.956552;
+    public static final double INITIAL_LONGITUDE = -53.459971;
+}
---
+++ app/src/vcsbc/java/br/com/lfdb/zup/core/Constantes.java
@@ -0,0 +1,16 @@
+package br.com.lfdb.zup.core;
+
+public class Constantes {
+
+    public static final String PREF_KEY_TWITTER_LOGIN = "twitterLogado";
+    public static final String PREF_KEY_OAUTH_TOKEN = "oauthToken";
+    public static final String PREF_KEY_OAUTH_SECRET = "oauthTokenSecret";
+
+    public static final String REST_URL = "http://vcsbc.saobernardo.sp.gov.br:8081";
+    public static final String WEBSITE_URL = "http://vcsbc.saobernardo.sp.gov.br";
+    public static final String PLACES_KEY = "AIzaSyCU4b3Ct3TydDXtLLxT38MXAhlf3OkYfrg";
+
+    public static final String NAMESPACE_DEFAULT = "1";
+    public static final double INITIAL_LATITUDE = -23.701878;
+    public static final double INITIAL_LONGITUDE = -46.561174;
+}
